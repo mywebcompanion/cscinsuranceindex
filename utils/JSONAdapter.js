1bf3c979b2d83649388acee8dd1f3b8c610a0c12
Devendra Desale <devendra.desale@gmail.com>
2016-01-21 13:17:18 +0800

    added data adapter for charts

--- utils/JSONAdapter.js
+++ utils/JSONAdapter.js
@@ -9,13 +9,13 @@ var JSONAdapter = {};
 
 JSONAdapter.metricDataAdapter = function(input) {
     var output = {};
-
     var metricType = {"Value": "ValueMetric", "Boolean": "BooleanMetric", "Rating": "RatingMetric"};
-
     _.each(input, function (object) {
         var metrics = {};
-        if (!output[object.market]) {
-            output[object.market] = {};
+        var market = object.market.toLowerCase();
+
+        if (!output[market]) {
+            output[market] = {};
         }
         _.each(object.metrics, function (metricsObj) {
             metricTypeObj = {};
@@ -28,33 +28,39 @@ JSONAdapter.metricDataAdapter = function(input) {
             metricTypeObj["name"] = metricsObj.name;
             metricTypeObj["value"] = metricsObj.value;
             metricTypeObj["weightage"] = 100;
-            if (metricType[metricsObj.type] === metricType["Value"]) {
+            metricTypeObj["rateorder"] = metricsObj.rateorder;
+            if (metricType[metricsObj.type] === metricType["Value"] && metricsObj.hasOwnProperty("benchmarkvalue")) {
                 metricTypeObj["benchmarkvalue"] = metricsObj.benchmarkvalue;
             }
             metrics[metricsObj.category][metricType[metricsObj.type]].push(metricTypeObj);
         });
-        _.each(object.recommendations, function(value,key){
-            metrics[key].Recommendations = value;
-        });
-        output[object.market][object.companyName] = metrics;
+        if(object.hasOwnProperty("recommendations")) {
+            _.each(object.recommendations, function(value,key){
+                metrics[key].Recommendations = value;
+            });
+        }
+        output[market][object.companyName] = metrics;
     });
 
-    // Part II
+     //Part II
     var out = _.each(output, function(country){
         _.each(country, function(company){
+            var numberOfCategories = 0;
+            var categoriesScore = 0;
             _.each(company, function(category){
                 var valueScore = 0;
                 var booleanScore = 0;
                 var ratingScore = 0;
                 _.each(category, function (value, type) {
-                    console.log("value:",value);
-                    console.log("type:", type);
-                    console.log("categories", category.length);
                     var score = 0;
                     switch (type) {
                         case "ValueMetric":
-                            for(i = 0; i < value.length; i++){
-                                score += value[i].value / value[i].benchmarkingbvalue * value[i].weightage;
+                            for (i = 0; i < value.length; i++) {
+                                if(value[i].hasOwnProperty('benchmarkvalue')) {
+                                    score += value[i].value / value[i].benchmarkvalue * value[i].weightage;
+                                } else{
+                                    score += value[i].weightage;
+                                }
                             }
                             if(value.length > 0)
                                 valueScore = score/value.length;
@@ -79,13 +85,65 @@ JSONAdapter.metricDataAdapter = function(input) {
                     }
                 });
                 category["score"] = (valueScore + booleanScore + ratingScore) / 3;
+                numberOfCategories += 1;
+                categoriesScore += category["score"];
             });
-            //company["score"] = 43;
+            if(numberOfCategories >0)
+                company["score"] = categoriesScore/numberOfCategories;
+            else
+                company["score"] = 0;
         });
     });
-    console.log(out);
     return out;
 };
 
-module.exports = JSONAdapter;
+JSONAdapter.metricDataGraphAdapter = function(input) {
+    var output = [];
+    var social = {"name":"Social Media", "data": [], "color" : "#EF2525"};
+    var cms = {"name":"CMS", "data": [], "color" : "#A51D13"};
+    var emailChat = {"name":"Email & chat", "data": [], "color" : "#FEB300"};
+    var analytics = {"name":"Analytics", "data": [], "color" : "#1A5F80"};
+    var seo = {"name": "SEO & ads", "data": [], "color": "#123222"};
+    var mobileUx = {"name":"Mobile & UX", "data": [], "color" : "#09395C"};
+    _.each(input, function(metrics, company ){
+        if(metrics.hasOwnProperty(social.name)){
+            social.data.push([company,Math.round(metrics[social.name].score) / 10]);
+        } else{
+            social.data.push([company,0]);
+        }
+        if(metrics.hasOwnProperty(cms.name)){
+            cms.data.push(Math.round(metrics[cms.name].score) / 10);
+        } else{
+            cms.data.push(0);
+        }
+        if(metrics.hasOwnProperty(seo.name)){
+            seo.data.push(Math.round(metrics[seo.name].score) / 10);
+        } else{
+            seo.data.push(0);
+        }
+        if(metrics.hasOwnProperty(emailChat.name)){
+            emailChat.data.push(Math.round(metrics[emailChat.name].score) / 10);
+        } else{
+            emailChat.data.push(0);
+        }
+        if(metrics.hasOwnProperty(mobileUx.name)){
+            mobileUx.data.push(Math.round(metrics[mobileUx.name].score) / 10);
+        } else{
+            mobileUx.data.push(0);
+        }
+        if(metrics.hasOwnProperty(analytics.name)){
+            analytics.data.push(Math.round(metrics[analytics.name].score) / 10);
+        } else{
+            analytics.data.push(0);
+        }
+    });
+    output.push(social);
+    output.push(cms);
+    output.push(seo);
+    output.push(emailChat);
+    output.push(mobileUx);
+    output.push(analytics);
+    return output;
+};
 
+module.exports = JSONAdapter;
