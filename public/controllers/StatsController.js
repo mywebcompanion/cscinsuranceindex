50d3ad21a6652a579f5d5cf9dd14cb126765e5c1
Arun <mywebcompanion@gmail.com>
2016-01-29 11:08:26 +0800

    changes for color theme

--- public/controllers/StatsController.js
+++ public/controllers/StatsController.js
@@ -63,4 +63,12 @@ InsuranceIndex.controller('StatsController', function($scope,UIMaster,ChartConfi
         lineWidth:3,
         lineCap:'circle'
     };
+    $scope.getRandomColor = function(){
+        var letters = '0123456789ABCDEF'.split('');
+        var color = '#';
+        for (var i = 0; i < 6; i++ ) {
+            color += letters[Math.floor(Math.random() * 16)];
+        }
+        return color;
+    }
 });
