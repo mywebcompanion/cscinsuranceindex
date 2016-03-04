6e1675d3704b837315bbd8d08fe0287484bc70d0
Arun <mywebcompanion@gmail.com>
2016-03-04 15:51:06 +0800

    Logo changes

--- utils/JSONAdapter.js
+++ utils/JSONAdapter.js
@@ -21,7 +21,9 @@ var JSONAdapter = {};
     _.each(input, function (object) {
         var metrics = {};
         var market = object.market.toLowerCase();
-        output.url = object.url.toLowerCase();
+        if(object.url){
+            output.url = object.url.toLowerCase();
+        }
 
         if (!output[market]) {
             output[market] = {};
